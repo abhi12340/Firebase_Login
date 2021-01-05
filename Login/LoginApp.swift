9800ec7de669144639615ba46ed6270c64245ea2
ktezi <tezikhan1@gmail.com>
2021-01-05 16:42:22 +0530

    Initial Commit

---
+++ Login/LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginApp.swift
+//  Login
+//
+//  Created by Abhishek Kumar on 05/01/21.
+//
+
+import SwiftUI
+
+@main
+struct LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
